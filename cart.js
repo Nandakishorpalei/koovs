0ce3bd52f3428c803de7de7e52583b01721ee56c
Nandakishorpalei <nandakishorpalei7676@gmail.com>
2021-12-12 13:32:48 +0530

    Update cart.js

--- cart.js
+++ cart.js
@@ -240,7 +240,7 @@ document.getElementById("promobtn").addEventListener("click",function(){
 })
 
 document.getElementById("lastbtn").addEventListener("click",function(){
-  window.location.href="orderSum.html";
+  window.location.href="ordersum.html";
 })
 
 var addresses = JSON.parse(localStorage.getItem("addressDetail")) || [];
